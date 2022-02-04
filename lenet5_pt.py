eb98adb9f5c6ea99093086581e2fc10d88809d3a
Lekolas01 <luka.leko1@gmail.com>
2022-02-04 18:04:15 +0100

    move LeNet5 to different file, add subset to traindataset to make it faster

--- lenet5_pt.py
+++ lenet5_pt.py
@@ -4,7 +4,9 @@ from datetime import datetime
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as data_utils
 from torch.utils.data import DataLoader
+from lenet5_model import LeNet5
 
 from torchvision import datasets, transforms
 
@@ -17,8 +19,9 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # parameters
 RANDOM_SEED = 42
 LEARNING_RATE = 0.001
-BATCH_SIZE = 32
-N_EPOCHS = 2
+BATCH_SIZE = 64
+N_TRAIN_SAMPLES = 1000
+N_EPOCHS = 20
 
 IMG_SIZE = 32
 N_CLASSES = 10
@@ -116,7 +119,7 @@ def validate(valid_loader, model, criterion, device):
         y_true = y_true.to(device)
 
         # Forward pass and record loss
-        y_hat, _ = model(X) 
+        y_hat, _ = model(X)
         loss = criterion(y_hat, y_true) 
         running_loss += loss.item() * X.size(0)
 
@@ -161,7 +164,7 @@ def training_loop(model, criterion, optimizer, train_loader, valid_loader, epoch
     
     return model, optimizer, (train_losses, valid_losses)
 
-# ## Data
+# Data
 
 # define transforms
 # transforms.ToTensor() automatically scales the images to [0,1] range
@@ -169,12 +172,14 @@ transforms = transforms.Compose([transforms.Resize((32, 32)),
                                  transforms.ToTensor()])
 
 # download and create datasets
-train_dataset = datasets.MNIST(root='../data/mnist_data', 
+train_dataset = datasets.MNIST(root='data/', 
                                train=True, 
                                transform=transforms,
                                download=True)
+train_dataset = data_utils.Subset(train_dataset, torch.arange(N_TRAIN_SAMPLES))
 
-valid_dataset = datasets.MNIST(root='../data/mnist_data', 
+
+valid_dataset = datasets.MNIST(root='data/', 
                                train=False, 
                                transform=transforms)
 
@@ -187,54 +192,23 @@ valid_loader = DataLoader(dataset=valid_dataset,
                           batch_size=BATCH_SIZE, 
                           shuffle=False)
 
-# **Plotting the images**
+# Plotting the images
 
-ROW_IMG = 10
-N_ROWS = 5
-
-def show_images(row_img, n_rows, dataset, cmap=None):
+def show_images(batch, title=None, cmap=None, row_size=8, n_rows=8):
+    n_items = n_rows * row_size
+    if batch.shape[0] < n_items: 
+        n_items = batch.shape[0] / row_size
     fig = plt.figure()
-    for index in range(1, ROW_IMG * N_ROWS + 1):
-        plt.subplot(N_ROWS, ROW_IMG, index)
+    for index in range(1, n_items + 1):
+        plt.subplot(n_rows, row_size, index)
         plt.axis('off')
-        plt.imshow(dataset[index], cmap=cmap)
-    fig.suptitle('MNIST Dataset - preview')
+        plt.imshow(batch[index - 1][0], cmap=cmap)
+    fig.suptitle(title)
     plt.show(block=False)
 
-show_images(ROW_IMG, N_ROWS, train_dataset.data, 'gray_r')
-
+show_images(next(iter(train_loader))[0], 'First Mini-Batch:', 'gray_r')
 
 # Implementing LeNet-5
-
-class LeNet5(nn.Module):
-
-    def __init__(self, n_classes):
-        super(LeNet5, self).__init__()
-        
-        self.feature_extractor = nn.Sequential(            
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
-            nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
-            nn.Tanh()
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84),
-            nn.Tanh(),
-            nn.Linear(in_features=84, out_features=n_classes),
-        )
-
-    def forward(self, x):
-        x = self.feature_extractor(x)
-        x = torch.flatten(x, 1)
-        logits = self.classifier(x)
-        probs = F.softmax(logits, dim=1)
-        return logits, probs
-
 
 torch.manual_seed(RANDOM_SEED)
 
