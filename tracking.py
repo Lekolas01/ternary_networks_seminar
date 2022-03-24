79e9963d6088860071eee92c42b1d13adcdc74ec
Lekolas01 <luka.leko1@gmail.com>
2022-03-24 11:26:26 +0100

    refactor logger

--- tracking.py
+++ tracking.py
@@ -18,17 +18,14 @@ class Logger:
         Parameters
         ----------
         log_every: int, optional
-            After how many epochs you want to log
+            After how many epochs you want to log.
         """
         self.log_every = log_every
 
-    def loop_init(self, model: nn.Module, train_loader: DataLoader, valid_loader: DataLoader, **kwargs):
-        self.model = model
-        self.train_loader = train_loader
-        self.valid_loader = valid_loader
+    def loop_init(self):
         pass
 
-    def log(self, **kwargs):
+    def log(self):
         """
         Log the loss and other metrics of the current mini-batch.
 
@@ -62,38 +59,33 @@ class Logger:
 class Progress(Logger):
     " Log progress of epoch to stdout. "
 
-    def __init__(self, **base_args):
+    def __init__(self, **kwargs):
         """
         Parameters
         ----------
         """
-        super().__init__(**base_args)
+        super().__init__(**kwargs)
 
-    def loop_init(self, epoch: int, starting_loss: float, **kwargs):
-        super().__init__()
+    def loop_init(self, **kwargs):
         super().loop_init(**kwargs)
-        self.device = next(self.model.parameters()).device
-        pass
 
-    def log(self, idx, model: TernaryModule, epoch, train_loss, valid_loss, train_acc, valid_acc):
-        weights = np.tanh(utils.get_all_weights(model).detach().cpu().numpy())
-        d, (minus_ones, zeros, plus_ones) = utils.distance_from_int_precision(weights)
+    def log(self, train_loss, train_acc, valid_acc, **kwargs):
+        weights = np.tanh(utils.get_all_weights(self.t.model).detach().cpu().numpy())
+        d, sparsity = utils.distance_from_int_precision(weights)
         distance = np.mean(d)
-        quantized_model = model.quantized().to(self.device)
-        q_valid_acc = utils.get_accuracy(quantized_model, self.valid_loader, self.device)
-        q_train_acc = utils.get_accuracy(quantized_model, self.train_loader, self.device)
-        temp = -1
-        # TODO: print quantized accuracy
+        quantized_model = self.t.model.quantized().to(self.t.device)
+        q_valid_acc = utils.get_accuracy(quantized_model, self.t.valid_loader, self.t.device)
+        q_train_acc = utils.get_accuracy(quantized_model, self.t.train_loader, self.t.device)
+
         print(f'{datetime.now().time().replace(microsecond=0)} --- '
-            f'Epoch: {epoch}\t'
-            #f'Train loss: {train_loss:.4f}\t'
-            #f'Valid loss: {valid_loss:.4f}\t'
+            f'Epoch: {self.t.epoch}\t'
             f'Train: {100 * train_acc:.4f}\t'
+            f'Loss: {train_loss:.4f}\t'
             f'Valid: {100 * valid_acc:.4f}\t'
             f'Q Train: {100 * q_train_acc:.4f}\t'
             f'Q Valid: {100 * q_valid_acc:.4f}\t'
             f'Distance: {distance:.4f}\t'
-            f'Sparsity: {zeros:.4f}\t')
+            f'Sparsity: {sparsity:.4f}\t')
         
     def log_summary(self):
         pass
@@ -139,10 +131,10 @@ class Plotter(Logger):
 
 class Results(Logger):
     ERROR_FILE = "errors.csv"
-    ERRORS_FORMAT = "{idx},{epoch},{tl:.4f},{vl:.4f},{ta:.4f},{va:.4f},{d:.4f},{m:.4f},{z:.4f},{p:.4f}\n"
+    ERRORS_FORMAT = "{idx},{epoch},{tl:.4f},{vl:.4f},{ta:.4f},{va:.4f},{d:.4f},{z:.4f}\n"
 
     CONFIGS_FILE = "configs.csv"
-    CONFIGS_FORMAT = "{seed},{lr},{batch_size},{samples},{ternary},{a},{b}\n"
+    CONFIGS_FORMAT = "{seed},{lr},{batch_size},{ternary},{a},{b}\n"
 
     MODEL_FILE = "config{idx:02d}_epoch{epoch:03d}.pth"
 
@@ -160,36 +152,35 @@ class Results(Logger):
 
         # prepare configs file
         with open(self.configs_path, 'w') as f:
-            f.write(self.CONFIGS_FORMAT.format(s='seed', lr='lr', bs='batch_size', sa='samples', t='ternary', a='a', b='b'))
+            f.write(self.CONFIGS_FORMAT.format(seed='seed', lr='lr', batch_size='batch_size', ternary='ternary', a='a', b='b'))
         
         # prepare errors file
         with open(self.errors_path, 'w') as f:
-            f.write('idx,epoch,train_loss,valid_loss,train_acc,valid_acc,distance,minus_ones,zeros,plus_ones\n')
+            f.write('idx,epoch,train_loss,valid_loss,train_acc,valid_acc,distance,sparsity\n')
 
 
-    def loop_init(self, idx: int, seed, lr, batch_size, samples, ternary, a, b, **kwargs):
+    def loop_init(self, **kwargs):
+        conf = self.t.conf
         # log the configuration
         with open(self.configs_path, 'a') as f:
-            f.write(self.CONFIGS_FORMAT.format(seed, lr, batch_size, samples, ternary, a, b))
+            f.write(self.CONFIGS_FORMAT.format(conf.seed, conf.lr, conf.batch_size, conf.ternary, conf.a, conf.b))
 
 
-    def log(self, idx, model, epoch, train_loss, valid_loss, train_acc, valid_acc):
-        weights = np.tanh(utils.get_all_weights(model).detach().cpu().numpy())
-        d, (minus_ones, zeros, plus_ones) = utils.distance_from_int_precision(weights)
+    def log(self, train_loss, valid_loss, train_acc, valid_acc):
+        weights = np.tanh(utils.get_all_weights(self.t.model).detach().cpu().numpy())
+        d, sparsity = utils.distance_from_int_precision(weights)
         distance = np.mean(d)
 
         # log errors
         log_line = self.ERRORS_FORMAT \
-            .format(idx=idx, epoch=epoch, tl=train_loss, vl=valid_loss, ta=train_acc, va=valid_acc, d=distance, m=minus_ones, z=zeros, p=plus_ones)
+            .format(idx=self.t.conf_idx, epoch=self.t.epoch, tl=train_loss, vl=valid_loss, ta=train_acc, va=valid_acc, d=distance, z=sparsity)
         with open(self.errors_path, 'a') as f:
             f.write(log_line)
 
-        if epoch % self.model_every == 0:
-            print(self.model_every, idx)
-            # log model
+        if self.t.epoch % self.model_every == 0:
             try:
-                curr_model_path = str(self.model_path).format(idx=idx, epoch=epoch)
-                torch.save(model, curr_model_path)
+                curr_model_path = str(self.model_path).format(idx=self.t.idx, epoch=self.t.epoch)
+                torch.save(self.t.model, curr_model_path)
             except Exception as inst:
                 print(f"Could not save model to {curr_model_path}: {inst}")
 
@@ -203,38 +194,40 @@ class Tracker:
         logger0, logger1, ... loggerN : Logger
             One or more loggers for logging training information.
         """
+        
         self.loggers = list(loggers)
-        self.idx = 0
+        self.conf_idx = 0
 
     def add_logger(self, logger: Logger):
         if (logger not in self.loggers):
             self.loggers.append(logger)
+            logger.t = self
 
 
-    def loop_init(self, model: nn.Module, train_loader: DataLoader, valid_loader: DataLoader, starting_loss: float, **kwargs):
+    def loop_init(self, model: nn.Module, train_loader: DataLoader, valid_loader: DataLoader, conf: Configuration, **kwargs):
         self.model = model
+        self.device = next(self.model.parameters()).device
         self.train_loader = train_loader
         self.valid_loader = valid_loader
-        self.idx += 1
+        self.conf = conf
+        self.conf_idx += 1
         self.epoch = 0
         self.train_losses = []
         self.valid_losses = []
 
         for logger in self.loggers:
-            logger.loop_init(self.epoch, starting_loss, model=self.model, train_loader=self.train_loader, valid_loader=self.valid_loader, **kwargs)
+            logger.loop_init(**kwargs)
 
 
     def track_loss(self, train_loss, valid_loss):
-        assert(isinstance(self.model, nn.Module))
         self.epoch += 1
 
-        device = next(self.model.parameters()).device
-        train_acc = utils.get_accuracy(self.model, self.train_loader, device)
-        valid_acc = utils.get_accuracy(self.model, self.valid_loader, device)
+        train_acc = utils.get_accuracy(self.model, self.train_loader, self.device)
+        valid_acc = utils.get_accuracy(self.model, self.valid_loader, self.device)
 
         for logger in self.loggers:
             if (self.epoch % logger.log_every == 0):
-                logger.log(self.idx, self.model, self.epoch, train_loss, valid_loss, train_acc, valid_acc)
+                logger.log(train_loss=train_loss, valid_loss=valid_loss, train_acc=train_acc, valid_acc=valid_acc)
         self.train_losses.append(train_loss)
         self.valid_losses.append(valid_loss)
 
