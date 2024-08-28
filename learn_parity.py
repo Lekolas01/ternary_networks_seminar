fix numerical precision bug, add custom_score

learn_parity.py
@@ -1,5 +1,6 @@
 import os
 from argparse import ArgumentParser, Namespace
+from distutils.sysconfig import customize_compiler
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,13 +10,24 @@ import torch
 import wittgenstein as lw
 from pandas import DataFrame, Series
 from sklearn import tree
-from sklearn.model_selection import RandomizedSearchCV, cross_val_score
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.model_selection import (
+    ParameterSampler,
+    RandomizedSearchCV,
+    cross_val_score,
+    train_test_split,
+)
 from sklearn.utils import shuffle
 
 from datasets import FileDataset, get_df
 from rule_extraction import nn_to_rule_set
 from rule_extraction_classifier import RuleExtractionClassifier
-from utilities import set_seed
+from utilities import accuracy, set_seed
+
+
+def custom_score(acc, complexity):
+    l = 0.05
+    return 1 / ((1.0 + l - acc) * complexity)
 
 
 def plot_decision_tree(clf_object, feature_names, class_names):
@@ -50,8 +62,9 @@ def training_runs(key):
     set_seed(seed)
 
     X, y = load_dataset(key)
-
-    max_epochs = 8000
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=seed
+    )
 
     runs = DataFrame(
         columns=["idx", "seed", "lr", "layer_width", "n_layer", "l1", "epochs", "wd"]
@@ -63,51 +76,54 @@ def training_runs(key):
         layer_width=3,
         n_layer=1,
         l1=3e-5,
-        epochs=max_epochs,
+        epochs=5000,
         wd=0.0,
         steepness=6,
-        delay=400,
+        delay=200,
     )
 
     # find best hyperparameter settings
     # abcdefg: Best parameters: {'l1': 1e-05, 'layer_width': 8, 'lr': 0.0003, 'n_layer': 1, 'steepness': 6}
     param_grid = {
-        "layer_width": [8],
-        "lr": [3e-4, 3e-3],
-        "l1": [1e-5],
-        "n_layer": [1, 2],
-        "steepness": [2, 6],
+        "layer_width": [5, 10],
+        "lr": [1e-4, 3e-3],
+        "l1": [1e-4, 3e-3],
+        "n_layer": [1, 2, 3],
+        "steepness": [2, 4, 8],
     }
 
-    random_search = RandomizedSearchCV(
-        re_clf,
-        param_grid,
-        scoring="accuracy",
-        n_iter=5,
-        cv=5,
-        error_score="raise",
-    )
+    n_iter = 8
+    # do a random search over the hyperparameter space
+    sampler = ParameterSampler(param_grid, n_iter=n_iter, random_state=seed + 1)
 
-    # random_search.fit(X, y)
-    # re_clf.set_params(**random_search.best_params_)
+    accs, complexities = np.zeros(n_iter), np.zeros(n_iter)
+    for i, sample in enumerate(sampler):
+        re_clf.set_params(**sample)
+        re_clf.fit(X_train, y_train)
 
+        accs[i] = accuracy_score(y_test, re_clf.predict(X_test))
+        complexities[i] = re_clf.bool_graph.complexity()
+
+    print(f"{accs = }")
+    print(f"{complexities = }")
+    print(f"{custom_score(accs, complexities)= }")
+    exit()
     # with these new-found hyperparameter settings, we do one final cross validation
-    # re_cv_scores = cross_val_score(
-    #     re_clf, X, y, cv=5, scoring="accuracy", error_score="raise"
-    # )
-    # print(f"cross validation scores: {re_cv_scores}")
+    re_cv_scores = cross_val_score(
+        re_clf, X, y, cv=5, scoring="accuracy", error_score="raise"
+    )
+    print(f"cross validation scores: {re_cv_scores}")
 
     # Best parameters:
     # abcdefg:
-    re_clf.set_params(l1=1e-03, layer_width=8, lr=0.003, n_layer=2, steepness=6)
-    print(re_clf.get_params())
-    print(f"Best parameters: {re_clf.get_params()}")
+    # re_clf.set_params(l1=1e-04, layer_width=8, lr=0.003, n_layer=2, steepness=6)
 
     # finally, we do a single fit on the whole dataset, and observe the learned rule set
     re_clf.fit(X, y)
-    print(f"Rule set: {str(re_clf.bool_graph)}")
-    print(f"{re_clf.q_ng = }")
+    # print(f"Rule set: {str(re_clf.bool_graph)}")
+    # print(f"{re_clf.q_ng = }")
     print(f"{re_clf.bool_graph.complexity() = }")
+    exit()
     return runs
 
 

rule_extraction.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 from matplotlib import pyplot as plt
 
 from neuron import NeuronGraph
-from q_neuron import QNG_from_QNN, QuantizedLayer, QuantizedNeuronGraph2
+from q_neuron import QNG_from_QNN, QuantizedLayer, QuantizedNeuronGraph
 from rule_set import RuleSetGraph
 
 
@@ -20,13 +20,13 @@ def nn_to_rule_set(model: nn.Sequential, df: pd.DataFrame):
         df.columns[:-1]
     )  # assume that df only has one target column at the far right
     data = {key: np.array(df[key], dtype=float) for key in keys}
-    q_neuron_graph: QuantizedNeuronGraph2
+    q_neuron_graph: QuantizedNeuronGraph
     if isinstance(model[0], nn.Linear):
         # transform the trained neural network to a directed graph of full-precision neurons
         neuron_graph = NeuronGraph.from_nn(model, keys)
         # transform the graph to a new graph of perceptrons with quantized step functions
         # q_neuron_graph = QuantizedNeuronGraph.from_neuron_graph(neuron_graph, data)
-        q_neuron_graph = QuantizedNeuronGraph2.from_neuron_graph(neuron_graph, data)
+        q_neuron_graph = QuantizedNeuronGraph.from_neuron_graph(neuron_graph, data)
     else:
         assert isinstance(model[0], QuantizedLayer)
         q_neuron_graph = QNG_from_QNN(model, keys)

rule_set.py
@@ -7,20 +7,11 @@ from typing import Tuple
 
 import numpy as np
 import pandas as pd
-import torch
-import torch.nn as nn
-from torch import isin
-from torch.utils.data.dataloader import DataLoader
 
 from bool_formula import NOT, Constant, Knowledge, Literal
 from neuron import bool_2_ch
 from node import Graph, Node
-from q_neuron import (
-    Perceptron,
-    QuantizedLayer,
-    QuantizedNeuronGraph,
-    QuantizedNeuronGraph2,
-)
+from q_neuron import Perceptron, QuantizedNeuronGraph
 from utilities import flatten
 
 
@@ -40,15 +31,14 @@ class Dp:
         assert 0 <= n_vars
         self.n_vars = n_vars
         self.data: list[list[DpNode]] = [[] for _ in range(self.n_vars + 1)]
-        self.eps = 1e-6
 
-    def find(self, k: int, val: float, eps=1e-6) -> DpNode | None:
+    def find(self, k: int, val: float) -> DpNode | None:
         assert k >= 0, f"k must be >= 0, but got {k}."
         if k > len(self.data):
             return None
         arr = self.data[k]
         for t in arr:
-            if t.min_thr + eps < val < t.max_thr - eps:
+            if t.min_thr < val < t.max_thr:
                 return t
         return None
 
@@ -58,7 +48,7 @@ class Dp:
             self.find(k, val.min_thr) is not None
             or self.find(k, val.max_thr) is not None
         ):
-            print(f"{self.data = }")
+            print(f"{self.data[k] = }")
             print(f"{k = }")
             print(f"{val = }")
             raise ValueError
@@ -237,7 +227,7 @@ class Subproblem:
 
 class RuleSetNeuron(Node):
     def __init__(
-        self, q_neuron: Perceptron, q_ng: QuantizedNeuronGraph2, simplify: bool
+        self, q_neuron: Perceptron, q_ng: QuantizedNeuronGraph, simplify: bool
     ) -> None:
         self.q_neuron = q_neuron
         self.q_ng = q_ng
@@ -375,7 +365,7 @@ class RuleSetNeuron(Node):
                         node.key, [IfThenRule(node.key, [], val=True)]
                     )
                 else:
-                    target_1 = dp.find(k + 1, node.mean, eps=0.0)
+                    target_1 = dp.find(k + 1, node.mean)
                     if target_1 is None:
                         print(f"{dp = }")
                         print(f"{k + 1 = }")
@@ -383,7 +373,7 @@ class RuleSetNeuron(Node):
                     if target_1 is None:
                         raise ValueError
 
-                    target_2 = dp.find(k + 1, node.mean + self.n_ins[k][1], eps=0.0)
+                    target_2 = dp.find(k + 1, node.mean + self.n_ins[k][1])
                     assert target_2 is not None
                     rule1 = IfThenRule(node.key, [(target_1.key, True)])
                     rule2 = IfThenRule(
@@ -445,7 +435,7 @@ class RuleSetGraph(Graph):
         super().__init__(rule_set_neurons)
 
     @classmethod
-    def from_QNG(cls, q_ng: QuantizedNeuronGraph2, simplify=True):
+    def from_QNG(cls, q_ng: QuantizedNeuronGraph, simplify=True):
         # keep track of the needed rule set neurons
         needed_rule_set_neurons: set[str] = {"target"}
         rule_set_neurons = []